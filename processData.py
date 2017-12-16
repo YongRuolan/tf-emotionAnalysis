aaf443ff448f531b0d2b33aaa879e1f449bf9236
YongRUolan <yongruolan@126.com>
2017-12-16 15:59:58 +0800

    sourceModel

---
+++ processData.py
@@ -0,0 +1,18 @@
+import os
+
+from numpy import unicode
+
+# path = "E:\weibo"
+# files = os.listdir(path)
+# rf = open("E:\charOfWeibo.txt",'w',encoding="utf-8")
+# index = 0
+# for file in files:
+#     f = open(path + "\\" +file, 'r', encoding = "utf-8")
+#     for line in f:
+#         index +=1
+#         if index%100000==0:
+#             print(index)
+#         text = unicode(line.strip())
+#         text = " ".join(text)
+#         rf.write(text+"\r")
+
